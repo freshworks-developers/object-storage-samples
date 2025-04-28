9e983cf341e5ea4cafbbac3c6e1b09d88ff8ef0a
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2025-04-28 21:56:54 +0530

    DEVREL-4481 Initial commit with working app

---
+++ app/scripts/app.js
@@ -0,0 +1,173 @@
+async function uploadFile() {
+  const fileInput = document.getElementById('fileInput');
+  const files = await fileInput.getFiles();
+  console.log(files);
+  const file = files[0];
+  const desc = document.getElementById("descInput").value;
+  const tag = document.getElementById("tagInput").value;
+  if (!file) return notify("alert", "Please choose a file!");
+
+  const formData = new FormData();
+  formData.append('file', file.file);
+  formData.append('description', desc);
+  formData.append("tag", tag);
+  formData.append('filename', file.name);
+  console.log(file);
+  console.log(formData);
+  try {
+    const response = await client.files.upload(formData);
+    console.info('File uploaded successfully');
+    console.info(response);
+    await loadFiles();
+  } catch (error) {
+    console.error('Error: Unable to upload file');
+    console.error(error);
+  }
+}
+
+async function viewFile(fileId) {
+  try {
+    const res = await client.files.get(fileId);
+    console.log(res);
+  } catch (e) {
+    console.error(e);
+    notify("danger", "Unable to open file");
+  }
+}
+
+async function downloadFile(fileId) {
+  try {
+    const res = await client.files.get(fileId);
+    window.open(res.download_url, "_blank");
+  } catch (e) {
+    console.error(e);
+    notify("danger", "Unable to open file");
+  }
+}
+
+async function deleteFile(fileId) {
+  try {
+    const result = await client.interface.trigger("showConfirm", {
+      message: "Are you sure you want to delete this file?",
+      saveLabel: "Yes",
+      cancelLabel: "No"
+    });
+    console.log(result);
+    if(result === "No") {
+      console.info("User cancelled the file delete operation");
+      return;
+    }
+    console.info("User confirmed the file delete operation");
+    try {
+      await client.files.delete(fileId);
+      notify("success", "File deleted");
+      loadFiles();
+    } catch (e) {
+      console.error(e);
+      notify("danger", "Delete failed");
+    }
+  } catch (error) {
+    console.error("Error: Failed showing confirmation dialog");
+    console.error(error);
+  }
+}
+
+async function loadFiles() {
+  const btnUploadFile = document.getElementById("btnUploadFile");
+  btnUploadFile.addEventListener("fwClick", uploadFile);
+  try {
+    const res = await client.files.list({});
+    const files = res.files || [];
+    const tbody = document.getElementById("fileList");
+    const noFilesDiv = document.getElementById("noFiles");
+    const filesTableDiv = document.getElementById("filesTable");
+
+    tbody.innerHTML = "";
+
+    if (files.length === 0) {
+      noFilesDiv.style.display = "block";
+      filesTableDiv.style.display = "none";
+      return;
+    }
+    noFilesDiv.style.display = "none";
+    filesTableDiv.style.display = "block";
+
+    files.forEach(file => {
+      const tr = document.createElement("tr");
+
+      tr.innerHTML = `
+        <td>${file.file_name}</td>
+        <td>${file.description || ""}</td>
+        <td>${(file.file_size_bytes / 1024).toFixed(1)} KB</td>
+        <td>${new Date(file.created_at).toLocaleString()}</td>
+        <td>${file.tag || ""}</td>
+        <td class="actions">
+          <fw-button data-action="view" data-file-id="${file.id}" color="primary">View</fw-button>
+          <fw-button data-action="download" data-file-id="${file.id}">Download</fw-button>
+          <fw-button data-action="delete" data-file-id="${file.id}" color="danger">Delete</fw-button>
+        </td>
+      `;
+      tbody.appendChild(tr);
+    });
+    
+    fileList.addEventListener('fwClick', async function (event) {
+            const target = event.target;
+            if (target.tagName.toLowerCase() === 'fw-button') {
+              const action = target.dataset.action;
+              const fileId = target.dataset.fileId;
+
+              if (action && fileId) {
+                if (action === 'view') {
+                  viewFile(fileId);
+                  try {
+                    let data = await client.interface.trigger('showModal', {
+                      title: 'Ticket Information',
+                      template: './views/modal.html',
+                      data: {
+                        file_id: fileId
+                      }
+                    });
+                    console.log(data); // success message
+                  } catch (error) {
+                      // failure operation
+                    console.error(error);
+                  }
+                } else if (action === 'download') {
+                  downloadFile(fileId);
+                } else if (action === 'delete') {
+                  deleteFile(fileId);
+                }
+              }
+            }
+    });
+  } catch (error) {
+    console.error(error);
+    notify("danger", "Failed to load files");
+  }
+}
+
+function notify(type, message) {
+  client.interface.trigger("showNotify", {
+    type: type,
+    message: message,
+  });
+}
+
+function onAppActivate() {
+  console.log("App activated");
+  loadFiles();
+  const btnUploadFile = document.getElementById("btnUploadFile");
+  btnUploadFile.addEventListener("fwClick", uploadFile);
+  console.log('btnUploadFile');
+  console.log(btnUploadFile);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  app.initialized().then(function (_client) {
+    window.client = _client;
+    client.events.on("app.activated", onAppActivate);
+  }).catch(function (error) {
+    console.error('Error: Failed to initialise the app');
+    console.error(error);
+  });
+});
